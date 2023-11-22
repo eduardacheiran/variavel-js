4abed9baef334efd5950bbad265d47ce3c15685e
eduardacheiran <133910877+eduardacheiran@users.noreply.github.com>
2023-11-22 15:40:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+
+let x = 10;
+let y = 10;
+ 
+console.log("Valores iniciais: x =", x, "e y =", y);
+ 
+let temp = x;
+x = y;
+y = temp;
+ 
+console.log("Novos valores: x =", x, "e y =", y);
+ 
+let numero1 = parseFloat(prompt("Insira o primeiro número:"));
+let numero2 = parseFloat(prompt("Insira o segundo número:"));
+ 
+let soma = numero1 + numero2;
+let multiplicacao = numero1 * numero2;
+ 
+console.log("A soma dos números é:", soma);
+console.log("A multiplicação dos números é:", multiplicacao);
+
+//
+
+let nome, cidade;
+ 
+console.log(typeof nome);
+console.log(typeof cidade);
+ 
+nome = prompt("Qual é o seu nome?");
+cidade = prompt("Em qual cidade você mora?");
+ 
+console.log(typeof nome);
+console.log(typeof cidade);
+ 
+console.log(`Olá, meu nome é ${nome} e eu moro em ${cidade}.`);
+ 
+//
+ 
+let perguntaUm = prompt('Você gosta de escutar música?')
+console.log('Você gosta de escutar música?', perguntaUm)
+ 
+let perguntaDois = prompt('Você gosta de ir a praia?')
+console.log('Você gosta de ir a praia?', perguntaDois)
+ 
+let perguntaTres = prompt('Você gosta de séries de suspense?')
+console.log('Você gosta de séries de suspense?', perguntaTres)
